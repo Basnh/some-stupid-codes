ef760e5e022a3155c9c9131f0e99c76f96360234
Basnh <129840468+Basnh@users.noreply.github.com>
2024-03-31 17:19:17 +0700

    Update Base code.h

--- CT177 - Cấu trúc dữ liệu /Danh sách đặc/Base code.h
+++ CT177 - Cấu trúc dữ liệu /Danh sách đặc/Base code.h
@@ -1,1 +1,80 @@
+#define Maxlength 50
+typedef int ElementType;
+typedef int Position;
+typedef struct {
+  ElementType Elements[Maxlength];
+  Position    Last;
+}List;
+
+
+void makeNull_List (List *L);                             //* hàm tạo danh sách rỗng
+int Empty_List (List L)                                   //* hàm trả về 1 nếu là danh sách rỗng, ngược lại trả về 0
+Position First_List (List L)                              //* hàm trả về vị trí phần tử đầu tiên trong danh sách L
+Position End_List (List L)                                //* hàm trả về vị trí phần tử cuối trong danh sách L
+Position Next (Position P, List L)                        //* hàm trả về vị trí sau vị trí P trong danh sách L
+void Insert_List (ElementType X, Position P, List *L)     //* hàm xen phần tử X vào vị trí P trong danh sách L
+void Delete_List (Position P, List *L)                    //* hàm xóa phần tử tại vị trí P trong danh sách L
+ElementType Retrieve(Position P, List L)                  //* hàm trả về nội dung phần tử tại vị trí P trong danh sách L
+Position Locate(ElementType X, List L)                    //* hàm trả về vị trí đầu tiên tìm thấy X trong trong danh sách L. Nếu không tìm thấy, trả về End_List
+
+
+
+
+void makeNull_List(List *L){
+    L->Last = 0;
+}
+
+int Empty_List (List L){
+    return (L.Last == 0) ? 1 : 0;
+}
+
+Position First_List (List L){
+    return 1;
+}
+
+Position End_List (List L){
+    return L.Last+1;
+}
+
+Position Next(Position P, List L) {
+  return P+1;
+}
+
+void Insert_List (ElementType X, Position P,List *L){
+    Position Q;
+    for (Q = L->Last; Q>P-1; Q--){
+        L->Elements[Q] = L->Elements[Q-1];}
+        L->Elements[P-1] = X;
+        L->Last++;
+}
+
+void Delete_List ( Position P,List *L){
+    Position Q;
+    for (Q=P; Q<L->Last; Q++){
+        L->Elements[Q-1] = L->Elements[Q];
+    }
+    L->Last--;
+}
+
+ElementType Retrieve (Position P,List L){
+    return L.Elements[P-1];
+}
+
+Position Locate (ElementType X,List L){
+    Position(P) = 1;
+    while (P!= L.Last+1){
+        if (L.Elements[P-1] == X)
+        return P;
+        else P++;
+    }
+    return P;
+}
+
+
+
+
+
+
+
+
 
