00d07260dfcf7ef1d12748b5ab0f61394703e52f
Basnh <129840468+Basnh@users.noreply.github.com>
2024-04-04 17:40:59 +0700

    Update Other code.h

--- CT177 - Cấu trúc dữ liệu /Danh sách liên kết/Other code.h
+++ CT177 - Cấu trúc dữ liệu /Danh sách liên kết/Other code.h
@@ -1,7 +1,8 @@
 double Average (List L)                          //* hàm trả về trung bình cộng của các phần tử trong danh sách L
 void DeleteX (ElementType X, List *L)            //* hàm xóa tất cả phần tử có giá trị bằng X trong danh sách L
-
-
+void filter (List *L)                            //* hàm xóa các phần tử trùng nhau trong danh sách L
+ElementType Max2_List (List L)                   //* hàm trả về giá trị lớn thứ hai trong các phần tử của danh sách L
+ElementType Min2_List (List L)                   //* hàm trả về giá trị nhỏ thứ hai trong các phần tử của danh sách L
 
 double Average (List L){
   if (L == NULL) {
@@ -38,4 +39,49 @@ void DeleteX(ElementType X, List *L) {
             P = P->Next;
         }
     }
+}
+
+void filter(List *L) {
+    Position current = First_List(*L);
+
+    while (current != End_List(*L)) {
+        ElementType currentElement = Retrieve(current, *L);
+        Position next = Next(current, *L);
+
+        while (next != End_List(*L)) {
+            ElementType nextElement = Retrieve(next, *L);
+            if (currentElement == nextElement) {
+                Delete_List(next, L);
+            } else {
+                next = Next(next, *L);
+            }
+        }
+        current = Next(current, *L);
+    }
+}
+
+ElementType Max2_List(List L){
+    Position P;
+    int max = Retrieve(First_List(L), L);
+    int max2 = 0;
+    for (P = First_List(L); P<End_List(L); P++){
+        if (Retrieve(P, L) > max){
+                max2 = max;
+                max = Retrieve(P, L);
+        }
+    }
+    return max2;
+}
+
+ElementType Min2_List(List L){
+    Position P;
+    int min = Retrieve(First_List(L), L);
+    int min2 = Retrieve(Next(First_List(L), L), L);
+    for (P = First_List(L); P<End_List(L); P++){
+        if (Retrieve(P, L) < min){
+                min2 = min;
+                min = Retrieve(P, L);
+        }
+    }
+    return min2;
 }
